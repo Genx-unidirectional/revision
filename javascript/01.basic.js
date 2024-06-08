ea3b50bea92c47c93263cb22c970e2e1ec7f739e
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-06-08 20:33:00 +0530

    feat numbers in js

---
+++ javascript/01.basic.js
@@ -0,0 +1,28 @@
+// Data types in javascript
+
+//Number//
+
+let num = 1;
+
+// static function in Number
+
+// console.log(Number.isSafeInteger(num));
+// console.log(Number.isFinite(num));
+// console.log(Number.isFinite(0));
+// console.log(Number.isInteger(num));
+// console.log(Number.isInteger(1.2));
+// console.log(Number.isNaN(num));
+
+// console.log(parseFloat("3434fmokfork"));
+// console.log(parseFloat("fmokfffork3434"));
+// console.log(parseFloat(0.3));
+// console.log(parseInt(11, 6));
+// console.log(parseInt("23"));
+
+//floating point number arithmetic operations
+
+// console.log(0.2 + 0.1 == 0.3);
+const num2 = 123.0303;
+const num3 = 2;
+// console.log(num2.toFixed(2));
+console.log(num3.toExponential(2));
