9191ee25a44b3882d1a31e7061b682d056ab95f0
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-07-01 11:32:09 +0530

    feat count vowels three ways

---
+++ practiceQuestion/build/js/12.countVowels.js
@@ -0,0 +1,38 @@
+"use strict";
+//Q-count vowels in the string
+function countVowels1(str) {
+    let count = 0;
+    for (let char of str) {
+        if (char === "a" ||
+            char === "e" ||
+            char === "i" ||
+            char === "o" ||
+            char === "u") {
+            count++;
+        }
+    }
+    return count;
+}
+function countVowels2(str) {
+    let count = 0;
+    let vowels = ["a", "e", "i", "o", "u"];
+    for (let char of str) {
+        if (vowels.includes(char)) {
+            count++;
+        }
+    }
+    return count;
+}
+// console.log(
+//   countVowels2("let have some thing so we do it before some things goes wrong")
+// );
+// console.log(
+//   countVowels1("let have some thing so we do it before some things goes wrong")
+// );
+function countVowels3(str) {
+    const reg = /[aeiou]/gi;
+    return str.match(reg)?.length;
+}
+// console.log(
+//   countVowels3("let have some thing so we do it before some things goes wrong")
+// );
