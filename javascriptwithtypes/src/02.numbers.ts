7513afba88327b3f328b20ce2af646f7a333d7c5
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-07-07 10:25:38 +0530

    feat numbers

---
+++ javascriptwithtypes/src/02.numbers.ts
@@ -0,0 +1,16 @@
+const num1 = 12;
+// console.log(Number.parseInt("2"));
+// console.log(Number.parseFloat("2.3344sdfoskfoo"));
+
+// console.log(Number.isFinite(12));
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MIN_SAFE_INTEGER);
+
+console.log(Number.isFinite("0"));
+
+const strNum = "234";
+console.log(parseInt(strNum));
+
+console.log(num1.toFixed(3));
+console.log(num1.toPrecision(3));
+console.log(num1.toExponential(2));
